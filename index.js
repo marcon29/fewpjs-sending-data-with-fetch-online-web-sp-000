fd447563e4ed2f1ac25072911adbbd433af78575
marcon29 <online@markstabler.com>
2022-04-01 10:02:01 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email){
+    let formData = {
+        name: name,
+        email: email
+    }
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },        
+        body: JSON.stringify(formData)
+    }
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(response => response.json())
+        .then(json => addUser(json))
+        .catch(error => addError(error));
+}
+
+function addUser(userInfo) {
+    document.body.innerHTML = userInfo.id;
+}
+
+function addError(error) {
+    document.body.innerHTML = error.message;
+}
